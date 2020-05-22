76a464c63ebaf8ac5b06895fb303f887d93a5740
Dmitry Hmel <dima.hmelios@gmail.com>
2020-05-22 20:43:13 +0300

    feat: update pug-slides-loader and add styles for highlight.js

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,18 @@
+/**
+ * In case you need to customize your slides order
+ * you can use this tag - <!--inject:order=0,1,2,3,4,5,-->
+ * and specify as many slides as you need to be rendered
+ * but becareful with spaces!
+ * Put this tag just below your inject tag.
+ * More info - https://www.npmjs.com/package/pug-slides-loader
+ */
+
 import Reveal from 'reveal.js';
 import 'sass/main.scss';
 
 document.body.innerHTML = `<div class="reveal">
 <div class="slides">
     <!--inject:slides-->
-    <section>
-      <img data-src="images/logo.webp">
-    </section>
-    <section>Slide 2</section>
 </div>
 </div>`;
 
